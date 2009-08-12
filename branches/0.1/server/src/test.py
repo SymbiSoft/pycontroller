19060ec2d143c00f6297cba18896773fa87aecfb
digabriel <digabriel@bf829370-85e5-11de-9bf0-63255af6188e>
2009-08-12 20:00:03 +0000

    --force-log

--- branches/0.1/server/src/test.py
+++ branches/0.1/server/src/test.py
@@ -1,4 +1,5 @@
 import lightblue
+import objc
 
 s = lightblue.socket()
 s.bind(("",0))
@@ -6,8 +7,17 @@ s.listen(1)
 lightblue.advertise("Test Drive", s , lightblue.RFCOMM)
 conn,addr = s.accept()
 print "Connected by", addr
+
+bndl = objc.loadBundle('CoreGraphics', globals(), '/System/Library/Frameworks/ApplicationServices.framework')
+objc.loadBundleFunctions(bndl, globals(), [('CGPostMouseEvent', 'v{CGPoint=ff}III')])
+
 while 1:
 	packet = conn.recv(1024)
-	print packet
-#conn.close()
-#s.close()
+	if packet == "167":
+        CGPostMouseEvent((300, 300), 1, 1, 1)
+        CGPostMouseEvent((300, 300), 1, 1, 0)
+	elif packet == "bye":
+		break
+		
+conn.close()
+s.close()
